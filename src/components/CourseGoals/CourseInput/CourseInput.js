2a286211e72fd0f193e363a6118cdf0274f36ff4
Rohit Dubey <Rohit.Dubey@odessainc.com>
2022-02-20 16:37:13 +0530

    Done

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,37 @@
 import React, { useState } from "react";
+// import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+// import "./CourseInput.css";
+
+import styles from "./CourseInput.module.css";
+
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color : ${props => props.invalid ? 'red' : 'black'};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+//     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'}
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -23,19 +53,27 @@ const CourseInput = (props) => {
     setEnteredValue("");
   };
 
-  
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${isValidInput ? '' : 'invalid'} `}>
-        <label>
-          Course Goal
-        </label>
+      {/* <FormControl className={`${isValidInput ? "" : "invalid"} `}> Normal Way of using Styled Component  */}
+      {/* Using props */}
+      {/* <FormControl invalid={!isValidInput}> */}
+      {/* <div className={`form-control ${isValidInput ? "" : "invalid"} `}> */}
+
+      {/* Using CSS Component */}
+      <div
+        className={`${styles["form-control"]}  ${
+          !isValidInput && styles.invalid
+        }`}
+      >
+        <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
           value={enteredValue}
         />
       </div>
+      {/* </FormControl> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,25 +1,34 @@
-import styled from "styled-components";
+import styles from "./Button.module.css";
+// this is how to use CSS Module in React and for this 
+// one we will have to rename Button.css file to Button.module.css
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// import styled from "styled-components";
 
-  &:focus {
-    outline: none;
-  }
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:hover,
-  active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
 // import './Button.css';
 
@@ -30,5 +39,13 @@ const Button = styled.button`
 //     </button>
 //   );
 // };
+
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
